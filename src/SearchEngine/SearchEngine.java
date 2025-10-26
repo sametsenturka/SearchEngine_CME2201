06b0f367292eec12f37f6b69cfdc09fa287d8f98
Samet Senturk <sametsenturk035@gmail.com>
2025-10-26 15:39:28 +0300

    Update SearchEngine.java

--- src/SearchEngine/SearchEngine.java
+++ src/SearchEngine/SearchEngine.java
@@ -3,17 +3,23 @@ package SearchEngine;
 import Data.ArticleLoader;
 import java.util.Scanner;
 
-public class SearchEngine {
+public class Main {
     public static void main(String[] args) {
 
         ArticleLoader articleLoader = new ArticleLoader();
+        Scanner scanner = new Scanner(System.in);
 
         articleLoader.loadStopWords("src/Data/stop_words_en.txt");
         articleLoader.loadDelimiters("src/Data/delimiters.txt");
         articleLoader.loadArticles("src/Data/CNN_Articels.csv");
         articleLoader.indexArticles();
 
-        articleLoader.searchArticles("[BURAYA ARAMANI YAZ]");
+        while(true){
+            System.out.print("SEARCH: ");
+            String searchQuery = scanner.nextLine();
+
+            articleLoader.searchArticles(searchQuery);
+        }
 
     }
 }
